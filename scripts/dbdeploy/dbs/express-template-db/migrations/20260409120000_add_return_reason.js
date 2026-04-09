b911ea378f7c5cbe771d0244a75eea0f19db4226
Muhammad Taufiq Febrianto <mtaufiqfebrianto6@gmail.com>
2026-04-09 12:07:56 +0800

    feat(db): add return_reason column to orders table

---
+++ scripts/dbdeploy/dbs/express-template-db/migrations/20260409120000_add_return_reason.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema.alterTable('orders', (table) => {
+    table.text('return_reason').nullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema.alterTable('orders', (table) => {
+    table.dropColumn('return_reason');
+  });
+};
